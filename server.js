4767d2673ed460b0124112f9fe18d5953a9327f0
Shameel Abdullah <shameel.abdullah@gmail.com>
2021-01-17 15:23:39 -0500

    typo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var cors_proxy = require("./lib/cors-anywhere");
 cors_proxy
   .createServer({
     originBlacklist: originBlacklist,
-    originWhitelist: ["https://stupefied-leavitt-b594cd.netlify.app/"],
+    originWhitelist: ["https://stupefied-leavitt-b594cd.netlify.app"],
     requireHeader: ["origin", "x-requested-with"],
     checkRateLimit: checkRateLimit,
     removeHeaders: [
